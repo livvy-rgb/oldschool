0bfa9e6487a36b28767f30e3ea4f7c361d27a415
Josh Cleland <jbxcleland@gmail.com>
2022-08-30 16:50:03 +1200

    Fix sideways scrolling through sections

--- new-social-enterprise-olivia-peterson-oldschool/js/app.js
+++ new-social-enterprise-olivia-peterson-oldschool/js/app.js
@@ -1,36 +1,32 @@
 const scroll = new LocomotiveScroll({
-    el: document.querySelector("[data-scroll-container]"),
-    smooth: true,
-    direction: "horizontal",
+  el: document.querySelector("[data-scroll-container]"),
+  smooth: true,
+  direction: "horizontal",
 });
 
-let blocks = document.querySelectorAll(".block[data-block-section]");
+let blocks = document.querySelectorAll(".block[section-indicator]");
 scroll.on("scroll", (args) => {
-    blocks.forEach((block) => {
-        let attr = block.getAttribute("data-block-section");
-        let section = document.querySelector(
-            `section[data-block-section='${attr}']`
-        );
+  blocks.forEach((block) => {
+    let blockNumber = block.getAttribute("section-indicator");
 
-        if (section.getBoundingClientRect().left <= block.offsetWidth * attr
-        ) {
-            block.classList.add("fixed");
-            block.classList.remove("init");
-            block.style.left = "";
-        } else if (
-            section.getBoundingClientRect().left >=
-            window.innerWidth - block.offsetWidth * (blocks.length - attr)
-        ) {
-            blocks.classList.add("init");
-            block.classList.remove("fixed");
-            block.style.left = "";
-        } else {
-            block.classList.remove("init");
-            block.classList.remove("fixed");
-        }
+    // Get corresponding data section
+    let dataSection = document.querySelector(
+      `section[data-block-section='${blockNumber}']`
+    );
 
-        if (block.classList.contains("active")) {
-            blocks.style.left = section.getBoundingClientRect().left + "px";
-        }
-    });
+    // If left side of the data section has entered view, switch classes from init to fixed
+    if (
+      dataSection.getBoundingClientRect().left <=
+      block.offsetWidth * blockNumber
+    ) {
+      block.classList.add("fixed");
+      block.classList.remove("init");
+    }
+
+    // If left side of data section has left view, switch classes from fixed to init
+    else {
+      block.classList.add("init");
+      block.classList.remove("fixed");
+    }
+  });
 });
